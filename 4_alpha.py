8c6fb69ff76d902f7f5d7c6fc2f5ae4a9812de0b
Mounia Engelbos <mounia.engelbos@hotmail.com>
2024-02-02 02:00:42 +0100

    à revoir

---
+++ 4_alpha.py
@@ -0,0 +1,15 @@
+#print(ord("z") - ord("q"))
+#print(122 - ord("z"))
+
+
+user_input = input("Entrez une lettre: ") #Demande de rentrer une lettre
+ascii_value = (ord("z") - ord(user_input))#Différence entre les nb des valeurs ascii de z et de la lettre entrée par l'utilisateur - le result renvoyé est un int
+print(ascii_value)                   #Il faudrait convertir le result(int) en string - (utiliser chr() -> jsp enfaite)
+                                          #Printer le result (string)
+                                          #En fonction du positionnement de la lettre remplir avec lettres de l'alphabet restant
+
+
+
+
+
+    
