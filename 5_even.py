9fa5aa7aa55bf17bfe2cc0f014a4a86ff37ed522
Mounia Engelbos <mounia.engelbos@hotmail.com>
2024-02-02 04:03:25 +0100

    à revoir

---
+++ 5_even.py
@@ -0,0 +1,7 @@
+user_input = int(input("Rentrez un nb: " ))
+if (user_input % 2) == 0 : 
+    print("pair")
+elif (user_input % 2) == 1:
+    print("impair")
+#else:
+    #print("Tu ne me la mettre pas à l'envers.")
